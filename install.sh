f86220397346717371edf4d8c2d752bac223c83f
Fabio M. Costa <fabiomcosta@gmail.com>
2012-03-02 13:10:38 -0300

    removing some unecessary lines, these synlinks are still into the repository

--- install.sh
+++ install.sh
@@ -13,15 +13,12 @@ pwd=$PWD
 git submodule update --init --recursive
 
 pushd vim/.vim/bundle
-make
+	make
 popd
 
-create_ln_if_not_yet_created "vim/.vim/snippets/django.snippets" "$pwd/vim/snipmate_for_django/snippets/django.snippets"
-create_ln_if_not_yet_created "vim/.vim/snippets/htmldjango.snippets" "$pwd/vim/snipmate_for_django/snippets/htmldjango.snippets"
-
 pushd $HOME
-create_ln_if_not_yet_created ".vim" "$pwd/vim/.vim"
-create_ln_if_not_yet_created ".vimrc" "$pwd/vim/.vimrc"
-create_ln_if_not_yet_created ".bash_profile" "$pwd/.bash_profile"
-
-source ".bash_profile"
+	create_ln_if_not_yet_created ".vim" "$pwd/vim/.vim"
+	create_ln_if_not_yet_created ".vimrc" "$pwd/vim/.vimrc"
+	create_ln_if_not_yet_created ".bash_profile" "$pwd/.bash_profile"
+	source ".bash_profile"
+popd
