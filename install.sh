031dcd8056591039adc26917961800beabe7f087
Fabio M. Costa <fabiomcosta@gmail.com>
2012-03-03 17:29:49 -0300

    cloning the vundle repo when runing the install script

--- install.sh
+++ install.sh
@@ -12,13 +12,12 @@ pwd=$PWD
 
 git submodule update --init --recursive
 
-pushd vim/.vim/bundle
-	make
-popd
-
 pushd $HOME
 	create_ln_if_not_yet_created ".vim" "$pwd/vim/.vim"
 	create_ln_if_not_yet_created ".vimrc" "$pwd/vim/.vimrc"
 	create_ln_if_not_yet_created ".bash_profile" "$pwd/.bash_profile"
 	source ".bash_profile"
 popd
+
+# clone the vundle plugin, to manage vim plugins
+git clone https://github.com/gmarik/vundle.git $HOME/.vim/bundle/vundle
