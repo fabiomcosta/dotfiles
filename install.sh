f7582c2965df1b1fc42ff5170ea120995239ee0f
Fabio M. Costa <fabiomcosta@gmail.com>
2012-11-10 16:50:15 -0500

    highlighting the important parts of the messages

--- install.sh
+++ install.sh
@@ -4,18 +4,23 @@ OK=`printf "\033[1;32m✓\033[0m"`
 WARNING=`printf "\033[1;33m⚠\033[0m"`
 ERROR=`printf "\033[1;31m✖\033[0m"`
 
+# highlights values
+hl() {
+    printf "\033[1;37m$1\033[0m"
+}
+
 create_ln_for() {
     if [ ! -e "$1" ]; then
         ln -s "$2" "$1"
     elif [ -L "$1" ]; then
-        echo "${OK} Symlink for \"$1\" was already created."
+        echo "${OK} Symlink for `hl $1` was already created."
     else
         if [ -f "$1" ]; then
-            echo "${WARNING} There is already a \"$1\" file inside your home folder."
+            echo "${WARNING} There is already a `hl $1` file inside your home folder."
         elif [ -d "$1" ]; then
-            echo "${WARNING} There is already a \"$1\" folder inside your home folder."
+            echo "${WARNING} There is already a `hl $1` folder inside your home folder."
         else
-            echo "${ERROR} \"$1\" isn't a symlink nor a folder nor a file. Do something!"
+            echo "${ERROR} `hl $1` isn't a symlink nor a folder nor a file. Do something!"
             exit 1
         fi
     fi
@@ -25,16 +30,16 @@ install() {
     command=$1
     shift
     if [ ! `which $command` ]; then
-        echo "Installing $command..."
+        echo "Installing `hl $command`..."
         $@
         if [[ "$?" ]]; then
-            echo "${ERROR} a problem happened while installing $command."
+            echo "${ERROR} a problem happened while installing `hl $command`."
             exit 1
         else
-            echo "${OK} $command successfully installed."
+            echo "${OK} `hl $command` successfully installed."
         fi
     else
-        echo "${OK} $command is already installed."
+        echo "${OK} `hl $command` is already installed."
     fi
 }
 
@@ -53,26 +58,27 @@ install brew ruby -e "$(curl -fsSkL raw.github.com/mxcl/homebrew/go)"
 install mvim brew install macvim --with-lua --override-system-vim
 install git brew install git
 
+
 # clone the vundle plugin, to manage vim plugins
 if [ ! -e $HOME/.vim/bundle/vundle ]; then
-    echo "Installing vundle..."
+    echo "Installing `hl 'vundle'`..."
     git clone https://github.com/gmarik/vundle.git $HOME/.vim/bundle/vundle
 else
-    echo "${OK} vundle is already installed."
+    echo "${OK} `hl 'vundle'` is already installed."
 fi
 
 if [ ! `which mvim` ]; then
-    echo "Installing/Updating macvim's plugins..."
+    echo "Installing/Updating `hl "macvim's plugins"`..."
     mvim +BundleInstall! +qall
     if [[ "$?" ]]; then
-        echo "${ERROR} We had a problem while updating macvim's plugins.";
+        echo "${ERROR} We had a problem while updating `hl "macvim's plugins"`.";
         exit 1
     else
-        echo "${OK} macvim's plugins updated successfuly.";
+        echo "${OK} `hl "macvim's plugins"` updated successfuly.";
     fi
 fi
 
 pushd $HOME &> /dev/null
-    echo "Sourcing .bash_profile...";
+    echo "Sourcing `hl ".bash_profile"`...";
     source ".bash_profile"
 popd &> /dev/null
