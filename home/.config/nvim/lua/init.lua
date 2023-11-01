04993493f6a5701b5929b4be51e11b03ae43998d
Fabio M. Costa <fabiomcosta@gmail.com>
2023-11-01 15:01:13 -0400

    setting ft to flow, when that makes sense

--- home/.config/nvim/lua/init.lua
+++ home/.config/nvim/lua/init.lua
@@ -1,12 +1,12 @@
 local IS_META_SERVER = (function()
   local hostname = vim.loop.os_gethostname()
   return vim.endswith(hostname, '.fbinfra.net')
-      or vim.endswith(hostname, '.facebook.com')
+    or vim.endswith(hostname, '.facebook.com')
 end)()
 
 -- would be nice to make this async, lazy and memoized
 local IS_ARC_ROOT = IS_META_SERVER
-    and vim.fn.system({ 'arc', 'get-config', 'project_id' }) ~= ''
+  and vim.fn.system({ 'arc', 'get-config', 'project_id' }) ~= ''
 
 local set_keymap = vim.api.nvim_set_keymap
 
@@ -201,7 +201,6 @@ set_keymap('n', '<LEADER>tn', ':tabnext<CR>', { noremap = true })
 -- avoid going on ex mode
 set_keymap('n', 'Q', '<NOP>', { noremap = true })
 
-
 -- Keeps selection when changing indentation
 -- https://github.com/mhinz/vim-galore#dont-lose-selection-when-shifting-sidewards
 set_keymap('x', '<', '<gv', { noremap = true })
@@ -232,20 +231,29 @@ end)
 
 vim.filetype.add({
   extension = {
-    php = function(path, bufnr)
+    php = function(_path, bufnr)
       if vim.startswith(vim.filetype.getlines(bufnr, 1), '<?hh') then
-        return 'hack', function(bufnr)
-          vim.opt_local.syntax = 'php'
-          vim.opt_local.iskeyword:append('$')
-          vim.opt_local.commentstring = "/* %s */"
-        end
+        return 'hack',
+          function(_bufnr)
+            vim.opt_local.syntax = 'php'
+            vim.opt_local.iskeyword:append('$')
+            vim.opt_local.commentstring = '/* %s */'
+          end
       end
       return 'php'
     end,
+    js = function(_path, bufnr)
+      for _, line in ipairs(vim.filetype.getlines(bufnr, 1, 16)) do
+        if string.find(line, '@flow') then
+          return 'flow'
+        end
+      end
+      return 'javascript'
+    end,
   },
   pattern = {
-    ['.*%.js.flow'] = 'javascript',
-  }
+    ['.*%.js.flow'] = 'flow',
+  },
 })
 
 local lazypath = vim.fn.stdpath('data') .. '/lazy/lazy.nvim'
@@ -374,7 +382,7 @@ require('lazy').setup({
     -- This plugin already constains 'tpope/vim-sleuth'
     'sheerun/vim-polyglot',
     init = function()
-      vim.g.polyglot_disabled = {'ftdetect', 'sensible'}
+      vim.g.polyglot_disabled = { 'ftdetect', 'sensible' }
     end,
     config = function()
       vim.g.javascript_plugin_flow = 1
@@ -688,7 +696,7 @@ require('lazy').setup({
         table.insert(servers, 'hhvm')
 
         local installed_extensions =
-            require('meta.lsp.extensions').get_installed_extensions()
+          require('meta.lsp.extensions').get_installed_extensions()
         if installed_extensions['nuclide.prettier'] then
           table.insert(servers, 'prettier@meta')
         end
@@ -998,9 +1006,9 @@ require('lazy').setup({
       vim.g.workspace_autosave_untrailtabs = 0
 
       vim.g.workspace_session_directory =
-          vim.fn.expand(vim.fn.stdpath('data') .. '/sessions')
+        vim.fn.expand(vim.fn.stdpath('data') .. '/sessions')
       vim.g.workspace_undodir =
-          vim.fn.expand(vim.fn.stdpath('data') .. '/sessions/.undodir')
+        vim.fn.expand(vim.fn.stdpath('data') .. '/sessions/.undodir')
     end,
   },
   {
