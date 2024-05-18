c7a9604f60a672360bb96b98663f9108780f3f37
Fabio M. Costa <fabiomcosta@gmail.com>
2024-05-18 14:53:46 -0400

    delete rust tools, I dont really use this

--- home/.config/nvim/lua/init.lua
+++ home/.config/nvim/lua/init.lua
@@ -765,39 +765,6 @@ require('lazy').setup({
     end,
   },
   {
-    'simrat39/rust-tools.nvim',
-    dependencies = { 'neovim/nvim-lspconfig' },
-    config = function()
-      local rt = require('rust-tools')
-      rt.setup({
-        server = {
-          on_attach = function(_, bufnr)
-            -- Hover actions
-            vim.keymap.set(
-              'n',
-              '<C-space>',
-              rt.hover_actions.hover_actions,
-              { buffer = bufnr }
-            )
-            -- Code action groups
-            vim.keymap.set(
-              'n',
-              '<Leader>a',
-              rt.code_action_group.code_action_group,
-              { buffer = bufnr }
-            )
-            vim.keymap.set(
-              'n',
-              'gd',
-              vim.lsp.buf.definition,
-              { buffer = bufnr }
-            )
-          end,
-        },
-      })
-    end,
-  },
-  {
     'williamboman/mason.nvim',
     config = function()
       require('mason').setup()
