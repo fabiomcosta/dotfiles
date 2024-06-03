0113404ad8ede84de4eee8bf1f8747252a09a391
fabs <fabs@fb.com>
2024-06-03 16:56:58 -0400

    back to enabling none-ls

--- home/.config/nvim/lua/plugins/none.lua
+++ home/.config/nvim/lua/plugins/none.lua
@@ -3,8 +3,13 @@ local utils = require('utils')
 return {
   'nvimtools/none-ls.nvim',
   dependencies = { 'nvim-lua/plenary.nvim' },
-  enabled = not utils.is_meta_server(),
   config = function()
+    -- none-ls needs to be enabled because "meta" uses it, but we only want to
+    -- call setup localy, because mason only works localy.
+    if utils.is_meta_server() then
+      return
+    end
+
     local none = require('null-ls')
     none.setup({
       on_attach = utils.auto_format_on_save,
