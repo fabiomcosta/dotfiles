92ee7d550d3ca98e8bea95fa654dfab5e57cdd8a
fabs <fabs@fb.com>
2024-06-04 21:39:57 -0400

    [tk] going through all windows from last to first instead of of checking for the last one

--- home/.config/nvim/lua/plugins/test.lua
+++ home/.config/nvim/lua/plugins/test.lua
@@ -16,22 +16,23 @@ return {
       local max_width = vim.o.columns
       local max_height = vim.o.lines - 1 - vim.o.cmdheight
 
-      -- get winnr from last windows
-      local last_window_nr = vim.fn.winnr('$')
-      local window_width = vim.fn.winwidth(last_window_nr)
-      local window_height = vim.fn.winheight(last_window_nr)
+      for window_nr = vim.fn.winnr('$'), 0, -1 do
+        local window_nr = i
+        local window_width = vim.fn.winwidth(window_nr)
+        local window_height = vim.fn.winheight(window_nr)
 
-      local is_full_width = window_width == max_width
-      local is_partial_height = window_height < max_height
+        local is_full_width = window_width == max_width
+        local is_partial_height = window_height < max_height
 
-      if is_full_width and is_partial_height then
-        local last_window_id = vim.fn.win_getid(last_window_nr)
-        local win_info = vim.fn.getwininfo(last_window_id)[1]
-        if win_info.terminal == 1 then
-          -- Sticky size/position
-          vim.g['test#neovim#term_position'] = 'botright ' .. window_height
+        if is_full_width and is_partial_height then
+          local window_id = vim.fn.win_getid(window_nr)
+          local win_info = vim.fn.getwininfo(window_id)[1]
+          if win_info.terminal == 1 then
+            -- Sticky size/position
+            vim.g['test#neovim#term_position'] = 'botright ' .. window_height
+          end
+          return utils.replace_termcodes('<C-w>' .. window_nr .. 'c')
         end
-        return utils.replace_termcodes('<C-w>' .. last_window_nr .. 'c')
       end
       return ''
     end, { expr = true })
