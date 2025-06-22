bae06c03db4c46e474c29dfb456f499eb7867cf2
Fabio M. Costa <fabiomcosta@gmail.com>
2025-06-22 15:23:24 -0400

    updating blink and only setting meta sources when applicable

--- home/.config/nvim/lua/plugins/cmp.lua
+++ home/.config/nvim/lua/plugins/cmp.lua
@@ -1,13 +1,37 @@
 local utils = require('utils')
 
+local meta_opts = utils.is_meta_server() and {
+  sources = {
+    default = {
+      'meta_tags',
+      'meta_tasks',
+      'meta_revsub'
+    }
+  },
+  providers = {
+    meta_tags = {
+      name = 'MetaTags',
+      module = 'meta.cmp.tags',
+    },
+    meta_tasks = {
+      name = 'MetaTasks',
+      module = 'meta.cmp.tasks',
+    },
+    meta_revsub = {
+      name = 'MetaRevSub',
+      module = 'meta.cmp.revsub',
+    },
+  }
+} or {}
+
 return {
   'saghen/blink.cmp',
   dependencies = {
     'rafamadriz/friendly-snippets',
     'folke/lazydev.nvim',
   },
-  version = 'v0.11',
-  opts = {
+  version = 'v1.*',
+  opts = vim.tbl_deep_extend('force', {
     fuzzy = {
       prebuilt_binaries = {
         proxy = {
@@ -19,17 +43,17 @@ return {
     appearance = {
       use_nvim_cmp_as_default = true,
     },
+    cmdline = {
+      sources = {}
+    },
     sources = {
-      -- add lazydev to your completion providers
       default = {
+        -- add lazydev to your completion providers
         'lazydev',
         'lsp',
         'path',
         'snippets',
         'buffer',
-        'meta_tags',
-        'meta_tasks',
-        'meta_revsub'
       },
       providers = {
         lazydev = {
@@ -38,20 +62,7 @@ return {
           -- make lazydev completions top priority (see `:h blink.cmp`)
           score_offset = 100,
         },
-        meta_tags = {
-          name = 'MetaTags',
-          module = 'meta.cmp.tags',
-        },
-        meta_tasks = {
-          name = 'MetaTasks',
-          module = 'meta.cmp.tasks',
-        },
-        meta_revsub = {
-          name = 'MetaRevSub',
-          module = 'meta.cmp.revsub',
-        },
       },
-      cmdline = {},
     },
     completion = {
       documentation = {
@@ -61,5 +72,5 @@ return {
         },
       },
     },
-  },
+  }, meta_opts),
 }
