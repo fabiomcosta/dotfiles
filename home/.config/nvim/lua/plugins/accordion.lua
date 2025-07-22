d4bd50fcc205edb708e211f5105be50b1fc651d1
Fabio M. Costa <fabiomcosta@gmail.com>
2025-07-22 16:53:06 -0300

    finally

--- home/.config/nvim/lua/plugins/accordion.lua
+++ home/.config/nvim/lua/plugins/accordion.lua
@@ -5,13 +5,18 @@ return {
       -- This makes sure that accordion won't change the height of horizontal
       -- windows/buffers when it calls "wincmd =", and the same for us.
       vim.cmd([[autocmd WinNew * set winfixheight]])
-      -- TODO when autocmd is supported on lua we can try to move this to lua properly
-      vim.api.nvim_create_user_command('AccordionAutoResize', function()
-        vim.cmd(
-          [[execute ":AccordionAll " . string(floor(&columns/(&colorcolumn + 11)))]]
-        )
-      end, {})
-      vim.cmd([[autocmd VimEnter,VimResized * :AccordionAutoResize]])
+
+      -- This is actually causing a bug when opening the Telescope windows
+      -- and I can't figure out why, simply stopping to run this for now.
+
+      -- vim.api.nvim_create_autocmd({ 'VimEnter', 'VimResized' }, {
+      --   pattern = '*',
+      --   callback = function()
+      --     vim.cmd(
+      --       [[execute ":AccordionAll " . string(floor(&columns/(&colorcolumn + 11)))]]
+      --     )
+      --   end,
+      -- })
     end,
   },
 }
