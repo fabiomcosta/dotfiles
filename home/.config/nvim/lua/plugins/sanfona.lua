66210ee4b058ee40c98c1fcc3d57d0c675b34eb1
Fabio M. Costa <fabiomcosta@gmail.com>
2025-08-03 13:25:04 -0400

    using 80 if colorcolumn is not defined

--- home/.config/nvim/lua/plugins/sanfona.lua
+++ home/.config/nvim/lua/plugins/sanfona.lua
@@ -2,7 +2,7 @@ return {
   {
     'fabiomcosta/sanfona.nvim',
     opts = {
-      min_width = tonumber(vim.o.colorcolumn) + 11,
+      min_width = (tonumber(vim.o.colorcolumn) or 80) + 11,
     },
   },
 }
