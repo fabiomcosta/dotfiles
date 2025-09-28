8a0adb9fadf04cddea0631e99b59dd066288f733
Fabio M. Costa <fabiomcosta@gmail.com>
2025-09-28 11:52:52 -0400

    moved into work plugin

--- home/.config/nvim/lua/plugins/sanfona.lua
+++ home/.config/nvim/lua/plugins/sanfona.lua
@@ -9,7 +9,7 @@ return {
       sanfona.setup({
         min_width = (tonumber(vim.o.colorcolumn) or 80) + 11,
       })
-      vim.keymap.set('n', '<C-k>', sanfona.win_focus_up, {remap = true})
+      vim.keymap.set('n', '<C-k>', sanfona.win_focus_up, { remap = true })
     end,
   },
 }
