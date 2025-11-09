353de67a1de5ec8515cd619cf1b010b075eab6f2
fabs <fabs@fb.com>
2025-11-09 18:21:03 -0400

    dnd stuff

--- home/.config/nvim/lua/dnd.lua
+++ home/.config/nvim/lua/dnd.lua
@@ -1,23 +1,11 @@
-local FILEPROXY_URL = 'http://localhost:8092/fileproxy'
-local is_focused = true
 -- Simple pattern that looks for an absolute path
-local path_pattern = '^%/?[%w%._%-%/]+$'
-
--- Inferring cursor size from neovim node.
--- This is not accurate for all users, but should work for most.
--- GUI users might want to change this to use vim.opt.guicursor:get()
-local function get_cursor_size()
-  local mode = vim.fn.mode()
-  -- normal and visual modes have the ticker cursor
-  if mode == 'n' or mode == 'v' then
-    return 1
-  end
-  return 0
-end
-
-local function is_ssh_session()
-  return vim.env.SSH_CLIENT ~= nil
-end
+-- local PATH_PATTERN = '^%/?[%w%._%-%/]+$'
+local FILEPROXY_URL = 'http://localhost:8092/fileproxy'
+-- The dnd handlers are only going to work when the current buffer
+-- matches this pattern.
+local PATTERN = '*.commit.hg.txt'
+local _is_focused = true
+local _last_cursor_pos = nil
 
 local function url_encode(str)
   str = str:gsub('([^%w%-_%.%~])', function(c)
@@ -37,6 +25,129 @@ end
 local function tempname_for(path)
   local tmp_path = vim.fn.tempname()
   return vim.fn.join({ vim.fs.dirname(tmp_path), vim.fs.basename(path) }, '/')
+end
+
+-- Download file from the local fileproxy
+local function download_from_fileproxy(file_path, tmp_path, callback)
+  vim.system(
+    { 'curl', '-o', tmp_path, FILEPROXY_URL .. url_encode_path(file_path) },
+    {},
+    vim.schedule_wrap(function(obj)
+      if obj.code ~= 0 then
+        return vim.notify(
+          string.format(
+            [[Could not download "%s" from local file proxy.]],
+            file_path
+          ),
+          vim.log.levels.ERROR
+        )
+      end
+      callback(obj)
+    end)
+  )
+end
+
+local function px_upload(file_path, callback)
+  local stderr_data
+  local px_proc
+  px_proc = vim.system(
+    { 'px', 'upload', file_path },
+    {
+      text = true,
+      stdin = true,
+      stderr = vim.schedule_wrap(function(err, data)
+        -- When first running px it asks the user to paste the token
+        -- from a provided URL, this awkward prompts the user to that
+        -- and writes it back to the px process.
+        if err == nil and data ~= nil and stderr_data ~= data then
+          stderr_data = data
+          local token = vim.fn.input(data)
+          px_proc:write(token .. '\n')
+        end
+      end),
+    },
+    vim.schedule_wrap(function(obj)
+      if obj.code ~= 0 then
+        return vim.notify(
+          string.format([[Could not upload "%s" to pixelcloud.]], file_path),
+          vim.log.levels.ERROR
+        )
+      end
+      callback(obj)
+    end)
+  )
+end
+
+local function is_ssh_session()
+  return vim.env.SSH_CLIENT ~= nil
+end
+
+local function focus_events(focus_gained_callback, focus_lost_callback, config)
+  -- When neovim is running through SSH the focus events don't work similarly
+  -- to how they work localy.
+  -- Losing focus on the current window to another window on the client won't
+  -- trigger a FocusLost event, only when changing between tmux windows.
+  -- To fix that, chatgpt suggested using this workround which seems to work
+  -- with its own limitations.
+  if is_ssh_session() then
+    -- milliseconds for "lost focus" detection
+    -- CursorHold triggers after this
+    vim.o.updatetime = 1500
+    local is_focused = true
+    local function simulate_focus_gained()
+      if not is_focused then
+        is_focused = true
+        vim.schedule(focus_gained_callback)
+      end
+    end
+    local function simulate_focus_lost()
+      if is_focused then
+        is_focused = false
+        vim.schedule(focus_lost_callback)
+      end
+    end
+    vim.api.nvim_create_autocmd(
+      { 'CursorMoved', 'CursorMovedI' },
+      vim.tbl_deep_extend('force', config, {
+        callback = simulate_focus_gained,
+      })
+    )
+    vim.api.nvim_create_autocmd(
+      'CursorHold',
+      vim.tbl_deep_extend('force', config, {
+        callback = simulate_focus_lost,
+      })
+    )
+  else
+    vim.api.nvim_create_autocmd(
+      'FocusGained',
+      vim.tbl_deep_extend('force', config, {
+        callback = function()
+          focus_gained_callback()
+        end,
+      })
+    )
+    vim.api.nvim_create_autocmd(
+      'FocusLost',
+      vim.tbl_deep_extend('force', config, {
+        callback = function()
+          focus_lost_callback()
+        end,
+      })
+    )
+  end
+end
+
+-- Inferring cursor size from neovim node.
+-- This is not accurate for all users, but should work for most.
+-- GUI users might want to change this to use vim.opt.guicursor:get()
+local function get_cursor_size()
+  local mode = vim.fn.mode()
+  -- normal and visual modes have the ticker cursor
+  if mode == 'n' or mode == 'v' then
+    return 1
+  end
+  return 0
 end
 
 local function split_file_path(file_path)
@@ -68,10 +179,15 @@ local function replace_column_range(buf, line, start_col, end_col, replacement)
 end
 
 local function handle_drop()
+  local win_id = vim.api.nvim_get_current_win()
+  local cursor_pos = vim.api.nvim_win_get_cursor(win_id)
+
   -- In general the user needs to blur/focusout nvim in order to be able
   -- to drag-n-drop a file, and when dropping it the text is first updated
   -- before the FocusGained.
-  if is_focused then
+
+  if _is_focused then
+    _last_cursor_pos = cursor_pos
     return
   end
 
@@ -82,11 +198,16 @@ local function handle_drop()
     return
   end
 
-  local buf_id = 0
-  local win_id = vim.api.nvim_get_current_win()
-  local cursor_row, cursor_col = unpack(vim.api.nvim_win_get_cursor(win_id))
+  local initial_col
+  if _last_cursor_pos ~= nil then
+    initial_col = _last_cursor_pos[2] + 1
+  else
+    initial_col = first_slash_index
+  end
 
-  local file_path = line:sub(first_slash_index, cursor_col + get_cursor_size())
+  local cursor_row, cursor_col = unpack(cursor_pos)
+  local file_path =
+    vim.trim(line:sub(initial_col, cursor_col + get_cursor_size()))
   if file_path == '' then
     -- Ignore as chances are there was no file drag-n-drop event
     return
@@ -110,52 +231,34 @@ local function handle_drop()
     end
   end
 
+  local buf_id = vim.api.nvim_get_current_buf()
   -- Remove the pasted file paths from the buffer
   replace_column_range(
     buf_id,
     cursor_row - 1,
-    first_slash_index - 1,
+    initial_col - 1,
     cursor_col + get_cursor_size(),
     ''
   )
 
-  vim.notify('Uploading to pixelcloud ' .. vim.inspect(file_paths))
+  vim.notify('Uploading to pixelcloud ' .. vim.fn.join(file_paths, ', '))
   -- TODO an inline UI wi/Users/fabs/local.txtth "Uploading {file_path}..." message
-
   -- TODO it would be nice to keep the same order as the pasted files
   -- currently the first file to upload is pasted in the buffer.
-  for _, file_path in ipairs(file_paths) do
+  for i, file_path in ipairs(file_paths) do
     local tmp_path = tempname_for(file_path)
-
-    vim.system(
-      { 'curl', '-o', tmp_path, FILEPROXY_URL .. url_encode_path(file_path) },
-      {},
-      function(obj)
-        if obj.code ~= 0 then
-          return vim.notify(
-            string.format(
-              [[Could not download "%s" from proxy server.]],
-              file_path
-            ),
-            vim.log.levels.ERROR
-          )
-        end
-        vim.system(
-          { 'px', 'upload', tmp_path },
-          { text = true },
-          vim.schedule_wrap(function(obj)
-            local px_url = vim.trim(obj.stdout)
-            replace_column_range(
-              buf_id,
-              cursor_row - 1,
-              first_slash_index - 1,
-              first_slash_index - 1,
-              ' ' .. px_url
-            )
-          end)
+    download_from_fileproxy(file_path, tmp_path, function(obj)
+      px_upload(tmp_path, function(obj)
+        local px_url = vim.trim(obj.stdout)
+        replace_column_range(
+          buf_id,
+          cursor_row - 1,
+          initial_col - 1,
+          initial_col - 1,
+          ' ' .. px_url
         )
-      end
-    )
+      end)
+    end)
   end
 end
 
@@ -168,21 +271,15 @@ end
 function M.setup()
   local augroup = M.disable()
   vim.api.nvim_create_autocmd({ 'TextChangedI', 'TextChanged' }, {
+    pattern = PATTERN,
     group = augroup,
     callback = handle_drop,
   })
-  vim.api.nvim_create_autocmd('FocusGained', {
-    group = augroup,
-    callback = function()
-      is_focused = true
-    end,
-  })
-  vim.api.nvim_create_autocmd('FocusLost', {
-    group = augroup,
-    callback = function()
-      is_focused = false
-    end,
-  })
+  focus_events(function()
+    _is_focused = true
+  end, function()
+    _is_focused = false
+  end, { pattern = PATTERN, group = augroup })
 end
 
 return M
