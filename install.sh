082726529f4bdf93973eb36c8804606645d5c094
fabs <fabs@fb.com>
2025-11-24 14:46:39 -0400

    giving preference to yarn

--- install.sh
+++ install.sh
@@ -15,14 +15,13 @@ fi
 # TBH this is more like a reminder to run these, because they probably
 # won't work anyway.
 
-if command_exists ttls_forward_proxy_server; then
-  if command_exists npm; then
-    npm config set proxy http://fwdproxy:8080
-    npm config set https-proxy http://fwdproxy:8080
-  fi
+if command_exists fwdproxy-config; then
   if command_exists yarn; then
     yarn config set proxy http://fwdproxy:8080
     yarn config set https-proxy http://fwdproxy:8080
+  elif command_exists npm; then
+    npm config set proxy http://fwdproxy:8080
+    npm config set https-proxy http://fwdproxy:8080
   fi
 fi
 
