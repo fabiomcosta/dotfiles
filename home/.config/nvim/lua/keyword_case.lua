f021c58f9bbe57fcc50e8cbe968d7d3bd47ce663
Fabio M. Costa <fabiomcosta@gmail.com>
2025-12-13 12:00:56 -0500

    much simpler way to match a keyword using vim regexes and \k

--- home/.config/nvim/lua/keyword_case.lua
+++ home/.config/nvim/lua/keyword_case.lua
@@ -1,35 +1,23 @@
-local utils = require('utils')
+-- \k is a special vim regex character that matches a keyword, considering
+-- the iskeyword option.
+-- So this function will return if a character is a keyword as defined by the
+-- current buffer iskeyword option.
+local function match_keyword(_char)
+  return vim.fn.match(_char, '\\k') ~= -1
+end
 
-local function get_keywords()
-  local keywords = ''
-  for _, value in pairs(vim.opt.iskeyword:get()) do
-    if value == '@' then
-      keywords = keywords .. '%w'
-    elseif value == '@-@' then
-      -- Since @ matches a-zA-Z, to match the @ char @-@ can be used
-      keywords = keywords .. '@'
-    elseif #value == 1 then
-      keywords = keywords .. value
-    elseif value ~= '^' and value:sub(1, 1) == '^' then
-      -- Ignoring negated ranges
-    else
-      local start, _end = string.match(value, '([^-]+)-([^-]+)')
-      if start ~= nil then
-        local nstart = tonumber(start)
-        if nstart ~= nil then
-          start = string.char(nstart)
-        end
-        local nend = tonumber(_end)
-        if nend ~= nil then
-          _end = string.char(nend)
-        end
-        keywords = keywords .. start .. '-' .. _end
-      else
-        error('Unexpected value part of keywords option: ' .. value)
-      end
-    end
-  end
-  return keywords
+-- Escapes some characters to be used in lua regexes
+local function regex_escape(str)
+  return str
+      :gsub('%-', '%%-')
+      :gsub('%$', '%%$')
+      :gsub('%?', '%%?')
+      :gsub('%.', '%%.')
+      :gsub('%^', '%%^')
+      :gsub('%(', '%%(')
+      :gsub('%)', '%%)')
+      :gsub('%[', '%%[')
+      :gsub('%]', '%%]')
 end
 
 -- This is a very specific method to this function.
@@ -44,57 +32,44 @@ local function get_non_range_keywords()
     elseif value ~= '@' and #value == 1 then
       -- @ represents all alphanumerics and we don't want to add it as a
       -- matching character, but anything else is good.
-      keywords = keywords .. value
+      keywords = keywords .. regex_escape(value)
     end
   end
   return keywords
 end
 
--- Escapes some characters to be used in lua regexes
-local function regex_escape(str)
-  return str
-    :gsub('%$', '%%$')
-    :gsub('%?', '%%?')
-    :gsub('%.', '%%.')
-    :gsub('%^', '%%^')
-    :gsub('%(', '%%(')
-    :gsub('%)', '%%)')
-    :gsub('%[', '%%[')
-    :gsub('%]', '%%]')
-end
-
 local function is_snake_case(word)
-  local keywords = regex_escape(get_non_range_keywords())
+  local keywords = get_non_range_keywords()
   return string.find(word, '_')
-    and #word:gsub('[_%l%d' .. keywords .. ']+', '') == 0
+      and #word:gsub('[_%l%d' .. keywords .. ']+', '') == 0
 end
 
 local function is_upper_case(word)
-  local keywords = regex_escape(get_non_range_keywords())
+  local keywords = get_non_range_keywords()
   return string.find(word, '_')
-    and #word:gsub('[_%u%d' .. keywords .. ']+', '') == 0
+      and #word:gsub('[_%u%d' .. keywords .. ']+', '') == 0
 end
 
 local function is_kebab_case(word)
-  local keywords = regex_escape(get_non_range_keywords())
+  local keywords = get_non_range_keywords()
   return string.find(word, '-')
-    and #word:gsub('[-%l%d' .. keywords .. ']+', '') == 0
+      and #word:gsub('[-%l%d' .. keywords .. ']+', '') == 0
 end
 
 local function is_camel_case(word)
-  local keywords = regex_escape(get_non_range_keywords())
+  local keywords = get_non_range_keywords()
   local word_without_special_keywords = word:gsub('[' .. keywords .. ']+', '')
   return #word:gsub('[%l%u%d' .. keywords .. ']+', '') == 0
-    and #word:gsub('%l+', '') > 0
-    and word_without_special_keywords:match('^%l')
+      and #word:gsub('%l+', '') > 0
+      and word_without_special_keywords:match('^%l')
 end
 
 local function is_pascal_case(word)
-  local keywords = regex_escape(get_non_range_keywords())
+  local keywords = get_non_range_keywords()
   local word_without_special_keywords = word:gsub('[' .. keywords .. ']+', '')
   return #word:gsub('[%l%u%d' .. keywords .. ']+', '') == 0
-    and #word:gsub('%l+', '') > 0
-    and word_without_special_keywords:match('^%u')
+      and #word:gsub('%l+', '') > 0
+      and word_without_special_keywords:match('^%u')
 end
 
 local function to_snake_case(word)
@@ -106,7 +81,7 @@ local function to_snake_case(word)
   if string.find(word, '-') then
     return word:gsub('-', '_'):lower()
   end
-  local keywords = regex_escape(get_non_range_keywords())
+  local keywords = get_non_range_keywords()
   word = word:gsub('^[' .. keywords .. ']?%u', string.lower)
   word = word:gsub('(%u)', function(c)
     return '_' .. c:lower()
@@ -131,7 +106,7 @@ local function to_kebab_case(word)
 end
 
 local function to_pascal_case(word)
-  local keywords = regex_escape(get_non_range_keywords())
+  local keywords = get_non_range_keywords()
   word = to_camel_case(word):gsub('^[' .. keywords .. ']?%l', string.upper)
   return word
 end
@@ -146,7 +121,7 @@ local function cycle_case(word)
 
   if is_camel_case(word) then
     local ft_supports_kebab_case =
-      vim.tbl_contains(vim.opt.iskeyword:get(), '-')
+        vim.tbl_contains(vim.opt.iskeyword:get(), '-')
     if ft_supports_kebab_case then
       return to_kebab_case(word)
     else
@@ -169,22 +144,18 @@ local function replace_word_under_cursor(transformer)
   local row, col = unpack(vim.api.nvim_win_get_cursor(winnr))
   local line = vim.api.nvim_get_current_line()
 
-  local keywords = '[' .. regex_escape(get_keywords()) .. ']'
-
   -- +1 to make it 1 indexed and +1 to also check the current char under
   -- the cursor.
   local start_col = col + 2
   local end_col = col + 1
 
   while
-    start_col > 0 and line:sub(start_col - 1, start_col - 1):match(keywords)
+    start_col > 0 and match_keyword(line:sub(start_col - 1, start_col - 1))
   do
     start_col = start_col - 1
   end
 
-  while
-    end_col < #line and line:sub(end_col + 1, end_col + 1):match(keywords)
-  do
+  while end_col < #line and match_keyword(line:sub(end_col + 1, end_col + 1)) do
     end_col = end_col + 1
   end
 
