61feda25b2b157cc6de25ea692aa3d97f50e4bf7
fabs <fabs@fb.com>
2026-02-13 15:18:44 -0400

    neovim vscode support

--- home/.config/nvim/lua/init.lua
+++ home/.config/nvim/lua/init.lua
@@ -266,19 +266,21 @@ end)
 
 require('secrets.meta.dnd').setup()
 
-local lazypath = vim.fs.joinpath(vim.fn.stdpath('data'), 'lazy', 'lazy.nvim')
-if not vim.loop.fs_stat(lazypath) then
-  vim.fn.system({
-    'git',
-    'clone',
-    '--filter=blob:none',
-    'https://github.com/folke/lazy.nvim.git',
-    '--branch=stable', -- latest stable release
-    lazypath,
-  })
-end
-vim.opt.rtp:prepend(lazypath)
+if not vim.g.vscode then
+  local lazypath = vim.fs.joinpath(vim.fn.stdpath('data'), 'lazy', 'lazy.nvim')
+  if not vim.loop.fs_stat(lazypath) then
+    vim.fn.system({
+      'git',
+      'clone',
+      '--filter=blob:none',
+      'https://github.com/folke/lazy.nvim.git',
+      '--branch=stable', -- latest stable release
+      lazypath,
+    })
+  end
+  vim.opt.rtp:prepend(lazypath)
 
-require('lazy').setup({
-  { import = 'plugins' },
-}, { dev = { path = '~/Dev/nvim-plugins' } })
+  require('lazy').setup({
+    { import = 'plugins' },
+  }, { dev = { path = '~/Dev/nvim-plugins' } })
+end
