ea1f834800a53c99bb3a91f764d1d9c5c1512813
Fabio M. Costa <fabiomcosta@gmail.com>
2026-08-25 14:40:29 -0400

    changing my notes path to a place that makes more sense and deleting ai.lua plugin

--- home/.config/nvim/lua/plugins/notes.lua
+++ home/.config/nvim/lua/plugins/notes.lua
@@ -14,7 +14,7 @@ return {
     workspaces = {
       {
         name = 'personal',
-        path = '~/.config/notes/personal',
+        path = '~/.local/share/notes/personal',
       },
     },
   },
